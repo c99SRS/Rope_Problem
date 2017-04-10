b6cded1e04ee560d49a6bf3e2aeb9597e4b4e1f3
c99SRS <smruti.work@gmail.com>
2017-04-10 22:26:57 +0530

    new output added

--- rope_problem.java
+++ rope_problem.java
@@ -3,21 +3,24 @@ import java.lang.*;
 import java.io.*;
  
 
-class PMTest
+class KnabTest
 {
-private int minCost(int size, int[] arr) {
+public int minCost(int size, int[] arr) {
         int minCost = 0;
         PriorityQueue<Integer> queue = new PriorityQueue<>();
         for (int i = 0; i < size; i++) {
             queue.add(arr[i]);
         }
+        //System.out.println("  before que " + queue);
         while (queue.size() > 1) {
+
             int first = queue.remove();
             System.out.println("first " + first);
             int second = queue.remove();
             System.out.println("second " + second);
             int sum = first + second;
             minCost += sum;
+            //System.out.println("que before add " + queue);
             queue.add(sum);
             System.out.println("que " + queue);
         }
@@ -25,7 +28,7 @@ private int minCost(int size, int[] arr) {
     }
     public static void main (String[] args) throws java.lang.Exception
     {
-        PMTest connectNRopes = new PMTest();
+        KnabTest connectNRopes = new KnabTest();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the max no of ropes: ");
         int size = sc.nextInt();
